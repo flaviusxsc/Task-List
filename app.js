3d00ad7ae5c21003e80b43e4cc7650f64618a407
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2019-12-06 18:06:03 +0200

    add new items

--- app.js
+++ app.js
@@ -29,7 +29,18 @@ if (itemInput.value === ''){
 //Create li element 
 const li = document.createElement('li');
 li.className = 'collection-item';
+li.appendChild(document.createTextNode(itemInput.value));
+//Creat a new link element
+const link =document.createElement('a');
+link.className = 'delete-item secondary-content';
+link.innerHTML='<i class ="fa fa-remove"></i>';
+li.appendChild(link);
 
+//Append li to ul 
+itemList.appendChild(li);
+
+//Clear input 
+itemInput.value='';
 
   e.preventDefault();
 }

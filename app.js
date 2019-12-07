3d0dea43816b665f1f4a420e8e11e8be6304d0d0
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2019-12-07 10:37:09 +0200

    delete and filter functions functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Define variables
+// Define variables-------------------------------------------------------------
 
 const form = document.querySelector('#item-form');
 const itemList = document.querySelector('.collection');
@@ -7,18 +7,24 @@ const filter = document.querySelector('#filter');
 const itemInput = document.querySelector('#item');
 
 
-//Event Listener 
+//Event Listener ----------------------------------------------------------------
 
 loadEventlisteners();
 
 
 function loadEventlisteners(){
-
-
+  //Add item event
   form.addEventListener('submit',addItem);
+  //Remove Item event
+  itemList.addEventListener('click',removeItem)
+  //Clear Items Event 
+  clearBtn.addEventListener('click',clearItems)
+  //Filter Items
+  filter.addEventListener('keyup',filterItems)
+
 } 
 
-//Add item function 
+//Add item function----------------------------------------------------------------
 
 function addItem(e){
 if (itemInput.value === ''){
@@ -43,4 +49,40 @@ itemList.appendChild(li);
 itemInput.value='';
 
   e.preventDefault();
+}
+
+//Remove item function ---------------------------------------------------
+
+function removeItem(e){
+
+if(e.target.parentElement.classList.contains('delete-item')) {
+  e.target.parentElement.parentElement.remove();
+ 
+ }
+}
+
+//Clear items function----------------------------------------------------
+
+function clearItems(e){
+
+itemList.innerHTML='';
+
+} 
+
+//Filter items function-----------------------------------------------------
+
+function filterItems(e){
+
+  const text = e.target.value.toLowerCase();
+  
+  document.querySelectorAll('.collection-item').forEach(
+    function(e){
+  const item = e.firstChild.textContent;
+  if (item.toLowerCase().indexOf(text) != -1){
+     e.style.display = 'block';
+  } else{
+    e.style.display = 'none';
+  }
+    }
+  );
 }
